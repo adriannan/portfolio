0a7c69cf18a8ec77d2d212ffccbf97f2e8200980
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-09-15 15:46:01 +0200

    prlx bckg pos

--- script/script.js
+++ script/script.js
@@ -1,26 +1,26 @@
 // MOVING HERO TITTLE ON SCROLL
 function setTranslate(xPos, yPos, el) {
-  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+  // el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+  el.style.backgroundPosition = xPos + "px " + yPos + "px";
 }
 
 function scrollLoop() {
   let yScrollPosition = window.scrollY;
   if (yScrollPosition < 600) {
-    // setTranslate(yScrollPosition * -8, 0, document.querySelector("#hero__h2"));
-    // setTranslate(yScrollPosition * 10, 0, document.querySelector("#hero__h3"));
     setTranslate(
       0,
       yScrollPosition * -0.3,
       document.querySelector("#hero-parallax")
     );
-    setTranslate(0, yScrollPosition * 0.5, document.querySelector(".hero"));
   }
   setTranslate(
     0,
-    yScrollPosition * -0.3,
+    yScrollPosition * -0.3 + 500,
     document.querySelector("#skills-parallax")
   );
   requestAnimationFrame(scrollLoop);
 }
 
 window.addEventListener("DOMContentLoaded", scrollLoop);
+
+// setTranslate(0, yScrollPosition * -2, document.querySelector(".hero"));

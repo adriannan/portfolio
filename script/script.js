c3f04c62b47844678ec289c4c47a1ae69b7331b2
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-09-17 02:03:45 +0200

    prlx sections

--- script/script.js
+++ script/script.js
@@ -1,29 +1,38 @@
 // MOVING HERO TITTLE ON SCROLL
-function setTranslate(xPos, yPos, el) {
-  // el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+function setBckgPosition(xPos, yPos, el) {
   el.style.backgroundPosition = xPos + "px " + yPos + "px";
+}
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "%, " + yPos + "%, 0)";
 }
 
 function scrollLoop() {
   let yScrollPosition = window.scrollY;
   if (yScrollPosition < 600) {
-    setTranslate(
+    setBckgPosition(
       0,
       yScrollPosition * -0.3,
       document.querySelector("#hero-parallax")
     );
   }
   setTranslate(
+    yScrollPosition * -0.1,
+    yScrollPosition * -0.1,
+    document.querySelector("#hero")
+    // document.querySelector(".hero__title")
+  );
+  setBckgPosition(
     0,
-    yScrollPosition * -0.3 + 500,
+    yScrollPosition * -0.3 + 400,
     document.querySelector("#skills-parallax")
   );
-  setTranslate(
+  setBckgPosition(
     0,
     yScrollPosition * -0.3 + 800,
     document.querySelector("#projects-parallax")
   );
-  setTranslate(
+  setBckgPosition(
     0,
     yScrollPosition * -0.3 + 1300,
     document.querySelector("#contact-parallax")
@@ -35,4 +44,4 @@ Gator(window).on("scroll", scrollLoop);
 
 // window.addEventListener("DOMContentLoaded", scrollLoop);
 
-// setTranslate(0, yScrollPosition * -2, document.querySelector(".hero"));
+// setBckgPosition(0, yScrollPosition * -2, document.querySelector(".hero"));

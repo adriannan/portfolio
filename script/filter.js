cbbc49ee0db480b748185779ddd18cb8740d0fb3
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-09-18 23:01:47 +0200

    create nav

--- script/filter.js
+++ script/filter.js
@@ -2,22 +2,40 @@ const btnsPro = [...document.querySelectorAll("button[data-cat]")];
 const projects = [...document.querySelectorAll(".projects__item")];
 const projectsContent = document.querySelector(".projects__content");
 
-function clear() {
-  const siteProjects = projects.filter(pro => pro.classList.contains("site"));
+function clear(key) {
+  const keyProjects = projects.filter(pro => pro.classList.contains(key));
   projectsContent.textContent = "";
-  siteProjects.forEach(pro => {
+  keyProjects.forEach(pro => {
     projectsContent.appendChild(pro);
   });
 }
 const filter = e => {
   e.preventDefault();
   const cat = e.target.dataset.cat;
-  const filterProjects = projects.filter(pro => pro.classList.contains(cat));
-  projectsContent.textContent = "";
-  filterProjects.forEach(pro => {
-    projectsContent.appendChild(pro);
-  });
+  clear(cat);
+  btnsPro.forEach(btn => btn.classList.remove("projects__btn-active"));
+  e.path[0].classList.add("projects__btn-active");
 };
 
-clear();
+clear("site");
 btnsPro.forEach(btn => btn.addEventListener("click", filter));
+
+// function clear() {
+//   const siteProjects = projects.filter(pro => pro.classList.contains("site"));
+//   projectsContent.textContent = "";
+//   siteProjects.forEach(pro => {
+//     projectsContent.appendChild(pro);
+//   });
+// }
+// const filter = e => {
+//   e.preventDefault();
+//   const cat = e.target.dataset.cat;
+//   const filterProjects = projects.filter(pro => pro.classList.contains(cat));
+//   projectsContent.textContent = "";
+//   filterProjects.forEach(pro => {
+//     projectsContent.appendChild(pro);
+//   });
+// };
+
+// clear();
+// btnsPro.forEach(btn => btn.addEventListener("click", filter));
--- script/script.js
+++ script/script.js
@@ -1,3 +1,28 @@
+const navItem = [...document.querySelectorAll(".nav__item")];
+const nav = document.querySelector("#header-nav");
+const logo = document.querySelector(".header__logo");
+const aboutSec = document.querySelector("#about");
+
+function changeNav() {
+  if (aboutSec.getBoundingClientRect().top < window.innerHeight / 2) {
+    logo.style.visibility = "visible";
+    nav.classList.add("header__nav-left");
+    nav.classList.remove("header__nav-right");
+    navItem.forEach(item => {
+      item.classList.add("nav__item-left");
+      item.classList.remove("nav__item-right");
+    });
+  } else {
+    logo.style.visibility = "hidden";
+    nav.classList.remove("header__nav-left");
+    nav.classList.add("header__nav-right");
+    navItem.forEach(item => {
+      item.classList.remove("nav__item-left");
+      item.classList.add("nav__item-right");
+    });
+  }
+}
+
 // MOVING HERO TITTLE ON SCROLL
 function setBckgPosition(xPos, yPos, el) {
   el.style.backgroundPosition = xPos + "px " + yPos + "px";
@@ -9,34 +34,36 @@ function setTranslate(xPos, yPos, el) {
 
 function scrollLoop() {
   let yScrollPosition = window.scrollY;
-  if (yScrollPosition < 600) {
+  if (yScrollPosition < 1000) {
+    changeNav();
     setBckgPosition(
       0,
       yScrollPosition * -0.3,
       document.querySelector("#hero-parallax")
     );
+    setTranslate(
+      yScrollPosition * -0.1,
+      yScrollPosition * -0.1,
+      document.querySelector("#hero")
+      // document.querySelector(".hero__title")
+    );
+  } else {
+    setBckgPosition(
+      0,
+      yScrollPosition * -0.3 + 300,
+      document.querySelector("#skills-parallax")
+    );
+    setBckgPosition(
+      0,
+      yScrollPosition * -0.3 + 900,
+      document.querySelector("#projects-parallax")
+    );
+    setBckgPosition(
+      0,
+      yScrollPosition * -0.3 + 1300,
+      document.querySelector("#contact-parallax")
+    );
   }
-  setTranslate(
-    yScrollPosition * -0.1,
-    yScrollPosition * -0.1,
-    document.querySelector("#hero")
-    // document.querySelector(".hero__title")
-  );
-  setBckgPosition(
-    0,
-    yScrollPosition * -0.3 + 300,
-    document.querySelector("#skills-parallax")
-  );
-  setBckgPosition(
-    0,
-    yScrollPosition * -0.3 + 800,
-    document.querySelector("#projects-parallax")
-  );
-  setBckgPosition(
-    0,
-    yScrollPosition * -0.3 + 1100,
-    document.querySelector("#contact-parallax")
-  );
   requestAnimationFrame(scrollLoop);
 }
 
